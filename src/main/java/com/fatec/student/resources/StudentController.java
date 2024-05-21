5c3343515ade25aa5b99036c772282eaae0b10ae
IgorGabriel23 <igoorgns@gmail.com>
2024-05-21 11:28:15 -0300

    CRUD do estudante e teste de API

--- src/main/java/com/fatec/student/resources/StudentController.java
+++ src/main/java/com/fatec/student/resources/StudentController.java
@@ -3,12 +3,19 @@ package com.fatec.student.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fatec.student.entities.Student;
 import com.fatec.student.services.StudentService;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
 @RequestMapping("students")
@@ -22,4 +29,19 @@ public class StudentController {
         return studentService.getStudents();
     }
 
+    @GetMapping("{id}")
+    public Student getStudentById(@PathVariable int id){
+        return studentService.getStudentById(id);
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteStudentById(@PathVariable int id){
+        this.studentService.deleteStudentById(id);
+    }
+    
+    @PostMapping
+    public Student save(@RequestBody Student student){
+        return this.studentService.save(student);
+    }
+    
 }
